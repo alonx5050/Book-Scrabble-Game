76ab38017efd1ddcd51eda43e9e4d8c5b3167549
RoeeShemesh <132275088+RoeeShemesh@users.noreply.github.com>
2023-05-03 13:54:39 +0300

    Update BookScrabbleHandler.java

--- Server side/Book Scarbble/Server Side/BookScrabbleHandler.java
+++ Server side/Book Scarbble/Server Side/BookScrabbleHandler.java
@@ -1,5 +1,46 @@
+//written by: Roee Shemesh - 209035179
 package test;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+public class BookScrabbleHandler implements ClientHandler {
+    PrintWriter out;
+    Scanner in;
+    @Override
+    public void handleClient(InputStream inFromclient, OutputStream outToClient) {
+        out=new PrintWriter(outToClient);
+        in=new Scanner(inFromclient);
+        DictionaryManager dm = new DictionaryManager();
+        String[] afterSplit = in.next().split(",");
+        String funcChar = afterSplit[0];
+        String[] args = new String[afterSplit.length-1];
+        System.arraycopy(afterSplit, 1, args, 0, afterSplit.length-1);
+        if(!funcChar.equals("Q")&&!funcChar.equals("C")) {
+            out.println("not a valid pattern");
+            return;
+        }
+        if(funcChar.equals("Q"))
+        {
+            if(dm.query(args))
+                out.println("true");
+            else
+                out.println("false");
+        }
+        else
+        {
+            if(dm.challenge(args))
+                out.println("true");
+            else
+                out.println("false");
+        }
+        out.flush();
+    }
 
-public class BookScrabbleHandler {
+    @Override
+    public void close() {
+        in.close();
+        out.close();
+    }
 }
